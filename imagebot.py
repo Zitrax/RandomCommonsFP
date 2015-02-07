e76901cfdbc47968421824da2aee8259114fc0ab
Daniel Bengtsson <daniel@bengtssons.info>
2015-02-07 13:03:33 +0100

    Started with find image

--- imagebot.py
+++ imagebot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import argparse
+import requests
+import yaml
 from twython import Twython
 
 APP_KEY = 'd2qYJ5oe7Iul5uQhZj0Sdlmin'
@@ -14,8 +16,24 @@ def get_auth_url():
     return auth['auth_url']
 
 
+def find_image():
+    # First collect a list of all images and dump to disk
+    # Further uses can continue from where left off
+    payload = {"action": "query", "list": "categorymembers", "cmtype": "file", "format": "json", "continue": "",
+               "cmsort": "timestamp", "cmlimit": "100", "cmtitle": "Category:Featured_pictures_on_Wikimedia_Commons"}
+    r = requests.get("http://commons.wikimedia.org/w/api.php", params=payload)
+    data = r.json()
+    if "warnings" in data:
+        print("WARNINGS:")
+        print(yaml.dump(data["warnings"], default_flow_style=False))
+    print(yaml.dump(data["query"]["categorymembers"]))
+
 parser = argparse.ArgumentParser(description='Bot for tweeting featured pictures from wikimedia commons.')
 parser.add_argument('--auth_url', action="store_true", help='Retrieve an auth url')
+parser.add_argument('--find_image', action="store_true", help='Find a commons image')
 args = parser.parse_args()
 
-print(args)
+if args.auth_url:
+    print(get_auth_url())
+elif args.find_image:
+    find_image()

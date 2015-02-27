95728fae257f68777c1675261a31cb3b7ce4cc1c
Daniel Bengtsson <daniel@bengtssons.info>
2015-02-27 21:25:21 +0100

    Collect all images

--- imagebot.py
+++ imagebot.py
@@ -16,17 +16,41 @@ def get_auth_url():
     return auth['auth_url']
 
 
-def find_image():
-    # First collect a list of all images and dump to disk
-    # Further uses can continue from where left off
+def partial_image_list(cmcontinue=None):
     payload = {"action": "query", "list": "categorymembers", "cmtype": "file", "format": "json", "continue": "",
                "cmsort": "timestamp", "cmlimit": "100", "cmtitle": "Category:Featured_pictures_on_Wikimedia_Commons"}
-    r = requests.get("http://commons.wikimedia.org/w/api.php", params=payload)
+    if cmcontinue:
+        payload["cmcontinue"] = cmcontinue
+
+    headers = {'User-Agent': 'ImageBot/0.1 (daniel@bengtssons.info)'}
+    r = requests.get("http://commons.wikimedia.org/w/api.php", params=payload, headers=headers)
+    r.raise_for_status()
     data = r.json()
     if "warnings" in data:
         print("WARNINGS:")
         print(yaml.dump(data["warnings"], default_flow_style=False))
-    print(yaml.dump(data["query"]["categorymembers"]))
+    #print(yaml.dump(data, default_flow_style=False))
+
+    if "continue" in data:
+        cmcontinue = data["continue"]["cmcontinue"]
+    else:
+        cmcontinue = None
+    print(cmcontinue)
+    images = {i["pageid"]: i["title"] for i in data["query"]["categorymembers"]}
+    return cmcontinue, images
+
+
+def find_image():
+    # First collect a list of all images and dump to disk
+    # Further uses can continue from where left off
+    all_images = {}
+    cmcontinue, images = partial_image_list()
+    while True:
+        all_images.update(images)
+        if cmcontinue is None or images is None:
+            break
+        cmcontinue, images = partial_image_list(cmcontinue)
+    print(all_images)
 
 parser = argparse.ArgumentParser(description='Bot for tweeting featured pictures from wikimedia commons.')
 parser.add_argument('--auth_url', action="store_true", help='Retrieve an auth url')
